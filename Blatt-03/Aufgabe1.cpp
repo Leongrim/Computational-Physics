b581e39a7d7e129949f61435301b6ebf56f9b077
Leonard Wollenberg <Leonard.Wollenberg@tu-dortmund.de>
2016-05-03 17:52:47 +0200

    Aufgabe 1 und 2 Fertig

--- Blatt-03/Aufgabe1.cpp
+++ Blatt-03/Aufgabe1.cpp
@@ -12,12 +12,10 @@ double* Harmo( double* Ort ){
 	return Pointer;
 }
 
-void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* Kraftfeld(double*) ){
-	std::fstream Datei;
-	Datei.open("Ergebnisse/Ergebnis_A.txt" , std::ios::trunc | std::ios::out);
-	double *Y_n = new double[6];
-	double Breite = 1;
-	double Anzahl = Zeit / Breite;
+void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* Kraftfeld(double*) , std::fstream& Datei ){
+	double *Y_n = new double[7];
+	double Breite = 0.001;
+	int Anzahl = (int) Zeit / Breite;
 	for (int i = 0; i < 3; ++i)
 	{
 		Y_n[i] = Ort[i];
@@ -28,22 +26,25 @@ void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* 
 	double *Zwischenergebniss = new double[6];
 	double *K_1=new double[6];
 	double *K_2=new double[6];
+
+
+
 	double *K_3=new double[6];
 	double *K_4=new double[6];
 	for (int Zaehler = 0; Zaehler <= Anzahl; ++Zaehler)
 	{
 		for (int Komponente = 0; Komponente < 3; ++Komponente)
 		{
-			K_1[Komponente] =     Y_n[ Komponente + 3 ];
+			K_1[ Komponente ] =    Breite * Y_n[ Komponente + 3 ];
 		}
 		for( int Komponente = 0 ; Komponente < 3 ; ++Komponente)
 		{
-			K_1[Komponente+3] = Kraftfeld( Y_n )[ Komponente ];
+			K_1[ Komponente + 3 ] = Breite * Kraftfeld( Y_n )[ Komponente ];
 		}
 
 		for (int Komponente = 0; Komponente < 6; ++Komponente)
 		{
-			Zwischenergebniss[ Komponente ] = Y_n[ Komponente ]+ 1.0/2.0 * K_1[ Komponente ];
+			Zwischenergebniss[ Komponente ] = Y_n[ Komponente ] + 1.0/2.0 * K_1[ Komponente ];
 		}
 
 		for (int Komponente = 0; Komponente < 3; ++Komponente)
@@ -71,7 +72,7 @@ void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* 
 
 		for(int Komponente = 0 ; Komponente < 6 ; ++Komponente)
 		{
-			Zwischenergebniss[ Komponente ] = Y_n[ Komponente ] + 1.0/2.0 * K_3[ Komponente ];
+			Zwischenergebniss[ Komponente ] = Y_n[ Komponente ] +  K_3[ Komponente ];
 		}
 
 		for (int Komponente = 0; Komponente < 3; ++Komponente)
@@ -84,12 +85,17 @@ void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* 
 		}
 		for (int Komponente = 0; Komponente < 6; ++Komponente)
 		{
-			Y_n[Komponente] =+ 1.0/6.0*K_1[ Komponente ] + 1.0/3.0*K_2[ Komponente ] + 1.0/3.0*K_3[ Komponente ] + 1.0/6.0*K_4[ Komponente ];
+			Y_n[Komponente] = Y_n[Komponente] + 1.0/6.0*( K_1[ Komponente ] + 2.0*K_2[ Komponente ] + 2.0*K_3[ Komponente ] + K_4[ Komponente ]);
 		}
-		Datei << Zaehler*Breite << '\t';
-		for (int Komponente = 0; Komponente < 6; ++Komponente)
+		Y_n[6]=Breite*Zaehler;
+		for (int Komponente = 0; Komponente < 7; ++Komponente)
 		{
-		Datei 	<< Y_n[ Komponente ] << '\t';
+			if ( Komponente < 1)
+			{
+				Datei 	<< Y_n[ 6 ] << '\t';
+			}
+			else Datei 	<< Y_n[ Komponente - 1] << '\t';
+		
 		}
 		Datei << std::endl;
 	}
@@ -98,20 +104,39 @@ void Runge_Kutta( double Zeit , double *Ort , double *Geschwindigkeit , double* 
 
 int main(){
 
-
+	std::fstream Datei;
+	Datei.open( "Ergebnisse/Ergebnis_A.txt" , std::ios::trunc | std::ios::out);
 
 	double *Start_Ort = new double [3];
 	double *Start_Geschwindigkeit = new double [3];
 
 	Start_Ort[0] = 10.0;
-	Start_Ort[1] = 5.0;
+	Start_Ort[1] = 10.0;
 	Start_Ort[2] = 0.0;
 
-	Start_Geschwindigkeit[0]= 1.0;
-	Start_Geschwindigkeit[1]= -2.0;
-	Start_Geschwindigkeit[2]= 10.0;
+	Start_Geschwindigkeit[0] = 0.0;
+	Start_Geschwindigkeit[1] = 0.0;
+	Start_Geschwindigkeit[2] = 0.0;
 
 
-	Runge_Kutta( 10000 , Start_Ort , Start_Geschwindigkeit , Harmo );
+	Runge_Kutta( 10 , Start_Ort , Start_Geschwindigkeit , Harmo , Datei);
+
+	Datei.close();
+	Datei.open( "Ergebnisse/Ergebnis_B.txt" , std::ios::trunc | std::ios::out);
+
+	Start_Ort[0] = 10.0 ;
+	Start_Ort[1] = 0.0 ;
+	Start_Ort[2] = 0.0;
+
+	Start_Geschwindigkeit[0] =   0.0;
+	Start_Geschwindigkeit[1] =  10.0;
+	Start_Geschwindigkeit[0] = 0.0;
+
+	Runge_Kutta( 10 , Start_Ort , Start_Geschwindigkeit , Harmo , Datei);
+	Datei.close();
+
+
+
+
 	return 0;
 }
