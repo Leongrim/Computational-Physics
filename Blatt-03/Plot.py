eacbfeaf3786c648a5daae8a88701baaf338fba5
Leonard Wollenberg <Leonard.Wollenberg@tu-dortmund.de>
2016-05-03 17:54:54 +0200

    Grobe Plots Hinzugefügt

--- Blatt-03/Plot.py
+++ Blatt-03/Plot.py
@@ -1,3 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 from scipy.optimize import curve_fit
+
+
+Zeit , Ort_x , Ort_y , Ort_z , V_x ,V_y , V_z = np.genfromtxt( 'Ergebnisse/Ergebnis_A.txt' , unpack=True)
+
+plt.plot( Zeit , Ort_y ,'r-')
+plt.show()
+
+Zeit , Ort_x , Ort_y , Ort_z , V_x ,V_y , V_z = np.genfromtxt( 'Ergebnisse/Ergebnis_B.txt' , unpack=True)
+
+#fig = plt.figure()
+#ax = fig.add_subplot(111,projection='3d')
+
+plt.plot( Ort_x , Ort_y  ,'r-')
+plt.show()
