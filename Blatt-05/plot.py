b00b190308079e00143edb6e167354601119aabb
Leonard Wollenberg <Leonard.Wollenberg@tu-dortmund.de>
2016-05-19 10:33:06 +0200

    Rudimentärer Plot hinzugefügt

--- Blatt-05/plot.py
+++ Blatt-05/plot.py
@@ -1,3 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+
+
+Zeit , Energie_kin , Energie_pot , Energie_ges , Temperatur , V_S_X , V_S_Y = np.genfromtxt( 'Ergebnisse/Ergebnis_1.txt' , unpack=True)
+plt.plot( Zeit, Energie_ges , 'b-' )
+#plt.plot( Zeit , Energie_kin , 'r-')
+#plt.plot( Zeit , Energie_pot , 'g-')
+plt.grid()
+plt.savefig("Plots/Plot_1_Energie.pdf")
+plt.clf()
