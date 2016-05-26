b46c6be037a176aca1dd45e80f2a2386f03e2d20
Leonard Wollenberg <Leonard.Wollenberg@tu-dortmund.de>
2016-05-26 19:34:06 +0200

    Thermustat eingefügt

--- Blatt-05/Aufgabe1_Leo.cpp
+++ Blatt-05/Aufgabe1_Leo.cpp
@@ -3,6 +3,7 @@
 #include <cstdlib>
 #include <time.h>
 #include "fstream"
+#include <string>
 
 double Generator(double min , double max){
 	double Random = (double) rand()/ RAND_MAX;
@@ -127,11 +128,12 @@ void Wechselwirkung( double* Ort_X_Rest , double* Ort_Y_Rest , double Ort_X , do
 		}
 	}
 }
-void Paarverteilung( int Bins , double* Ort_X , double* Ort_Y , double* Paar , uint Teilchen , double Laenge){
+void Paarverteilung( int Bins , double* Ort_X , double* Ort_Y , double* Paar , uint Teilchen , double Laenge , double cutoff){
 	double Abstand_x;
 	double Abstand_y;
 	double Abstand;
-	double Breite = Laenge*0.5/Bins;
+	double Konst = Laenge*Laenge/(16*16);
+	double Breite = cutoff/Bins;
 	for (int n_x = -1; n_x < 2; ++n_x)
 	{
 		for (int n_y = -1; n_y < 2; ++n_y)
@@ -145,8 +147,10 @@ void Paarverteilung( int Bins , double* Ort_X , double* Ort_Y , double* Paar , u
 					Abstand = sqrt ( Abstand_x*Abstand_x + Abstand_y*Abstand_y);
 					for (int i = 1; i <= Bins ; ++i)
 					{
-						if( Breite * i < Abstand && Breite * ( 1 + i) > Abstand){
-							Paar[ i ] += 1.0/(( i * Breite)*( i * Breite)*( i * Breite) - ( i- 1)*( i - 1 ) * ( i - 1 ) * Breite * Breite * Breite);
+						if( Breite * (i - 1) < Abstand && Breite * ( i ) > Abstand){
+							Paar[ i ] += Konst/((double) i * Breite * i * Breite * i * Breite - ( i - 1) * Breite *( i - 1 ) * Breite * ( i - 1 ) * Breite );
+
+							break;
 						}
 					}
 				}
@@ -156,20 +160,30 @@ void Paarverteilung( int Bins , double* Ort_X , double* Ort_Y , double* Paar , u
 }
 
 
-void Verlet( double Breite , double Temp , double Laenge , double Zeit , uint Bins , double Messbegin , std::fstream& Energie_Datei , std::fstream& Orte_Datei  , std::fstream& OBS_Datei , std::fstream& Paar_Datei){
+void Verlet( double Breite , double Temp , double Laenge , double Zeit , uint Bins , double Messbegin , std::string Datei , bool Termustat = false){
+	std::fstream Energie_Datei;
+	std::fstream Orte_Datei;
+	std::fstream OBS_Datei;
+	std::fstream Paar_Datei;
+	Energie_Datei.open( (Datei + "_Energie.txt").data() , std::ios::trunc | std::ios::out);
+	Orte_Datei.open( (Datei + "_Orte.txt").data() , std::ios::trunc | std::ios::out);
+	OBS_Datei.open( (Datei + "_OBS.txt").data() , std::ios::trunc | std::ios::out);
+	Paar_Datei.open( (Datei + "_Paar.txt").data() , std::ios::trunc | std::ios::out);
 	int Anzahl = (int) ( Zeit / Breite ) ;
 	double Energie_kin;
 	double Energie_pot;
 	double Energie_kin_OBS = 0.0;
 	double Energie_pot_OBS = 0.0;
 	double Temp_OBS =0.0;
+	double Temperatur_ = Temp;
 	double V_S_X;
 	double V_S_Y;
 	double Ort_X[16];
 	double Ort_Y[16];
 	double V_X[16];
 	double V_Y[16];
-	Initialisierung( Ort_X , Ort_Y , V_X , V_Y , Temp , Laenge );
+	double Alpha;
+	Initialisierung( Ort_X , Ort_Y , V_X , V_Y , Temperatur_ , Laenge );
 	double Kraft_0_X[16];
 	double Kraft_1_X[16];
 	double Kraft_0_Y[16];
@@ -193,7 +207,7 @@ void Verlet( double Breite , double Temp , double Laenge , double Zeit , uint Bi
 	Temperatur( V_X , V_Y , Temp);
 	Energie_Datei << "#Zeit" << "\t\t" << "Energie_kin" << "\t\t" << "Energie_pot" << "\t\t" << "Energie_ges" << "\t\t" << "Temperatur" << "\t\t" << "V_S_X" <<"\t\t" << "V_S_Y" << std::endl;
 	Energie_Datei << 0 << "\t\t" << Energie_kin << "\t\t" << Energie_pot << "\t\t" << Energie_kin + Energie_pot << "\t\t" << Temp << "\t\t" << V_S_X <<"\t\t" << V_S_Y << std::endl;
-	for (int zaehler = 1; zaehler < Anzahl ; ++zaehler)
+	for (double zaehler = 1; zaehler < Anzahl ; ++zaehler)
 	{
 		for(int i = 0; i < 16; ++i)
 		{
@@ -219,117 +233,97 @@ void Verlet( double Breite , double Temp , double Laenge , double Zeit , uint Bi
 			Ort_X[ i ] = Ort_X[ i ] - Laenge * floor( Ort_X[ i ]/ Laenge);
 			Ort_Y[ i ] = Ort_Y[ i ] - Laenge * floor( Ort_Y[ i ]/ Laenge);
 		}
+		Temperatur( V_X , V_Y , Temperatur_);
+		if (Termustat)
+		{
+			Alpha = sqrt( Temp / Temperatur_);
+			for (int i = 0; i < 16; ++i)
+			{
+				V_X[ i ] = Alpha * V_X[ i ];
+				V_Y[ i ] = Alpha * V_Y[ i ];
+			}
+		}
 		E_kin( V_X , V_Y , Energie_kin );
 		E_pot( Ort_X , Ort_Y , Energie_pot , Laenge);
 		schwer_Geschw( V_X , V_Y , V_S_X , V_S_Y);
-		Temperatur( V_X , V_Y , Temp);
-		Energie_Datei << zaehler * Breite << "\t\t" << Energie_kin << "\t\t" << Energie_pot << "\t\t" << Energie_kin + Energie_pot << "\t\t" << Temp << "\t\t" << V_S_X <<"\t\t" << V_S_Y << std::endl;
+		Temperatur( V_X , V_Y , Temperatur_);
+		Energie_Datei << zaehler * Breite << "\t\t" << Energie_kin << "\t\t" << Energie_pot << "\t\t" << Energie_kin + Energie_pot << "\t\t" << Temperatur_ << "\t\t" << V_S_X <<"\t\t" << V_S_Y << std::endl;
 		for (int i = 0; i < 16; ++i)
 		{
 			Orte_Datei << Ort_X[ i ] << "\t" << Ort_Y[ i ] << "\t";
 		}
 		Energie_pot_OBS += Energie_pot;
-		Energie_pot_OBS += Energie_kin;
-		Temp_OBS += Temp;
-		OBS_Datei << Breite*zaehler << "\t" << Energie_pot_OBS/((double) (Breite * zaehler)) << "\t" << Energie_kin_OBS/((double) (Breite* zaehler)) << "\t" << Temp_OBS/((double) (Breite * zaehler)) << std::endl;
+		Energie_kin_OBS += Energie_kin;
+		Temp_OBS += Temperatur_;
+		OBS_Datei << Breite*zaehler << "\t" << Energie_kin_OBS/((double) ( zaehler)) << "\t" << Energie_pot_OBS/((double) (zaehler)) << "\t" << Temp_OBS/((double) ( zaehler)) << std::endl;
 		Orte_Datei << std::endl;
 		if (Messung < zaehler)
 		{
 			for (int i = 0; i < 16; ++i)
 			{	
-				Paarverteilung( Bins , Ort_X , Ort_Y , Paar , i , Laenge);
+				Paarverteilung( Bins , Ort_X , Ort_Y , Paar , i , Laenge , Laenge*0.5);
 			}
 			for (int i = 0; i < Bins; ++i)
 			{	
 				Paar_OBS[ i ] += Paar[ i ];
-				Paar_TIME[ i ] = Paar_OBS[ i ] /*/((double) Breite * zaehler - Messbegin)*/;
+				Paar_TIME[ i ] = Paar_OBS[ i ] /((double)  zaehler - Messung);
 				Paar[ i ] = 0;
 			}
 		}
 	}
 	for (int i = 0; i < Bins; ++i)
 	{
-		Paar_Datei << (double)i*Laenge*0.5/Bins << "\t" << Paar_TIME[ i ] << std::endl;
+		Paar_Datei << (double)(i )*Laenge*0.5/Bins << "\t" << Paar_TIME[ i ] << std::endl;
 	}
+	Paar_Datei.close();
+	Orte_Datei.close();
+	Energie_Datei.close();
+	OBS_Datei.close();
 }
 
 int main(){
-	srand (1233456);
-	std::fstream Datei;
+	srand (12);//1233456     12356
 
-	std::fstream Orte;
 
-	std::fstream OBS;
-
-	std::fstream PAAR;
-
+	std::string Datei("Ergebnisse/Ergebnis_");
 
 	uint Bins = 100;
 
 	double Tempe = 1.0;
 	double Laenge = 8.0;
-	double Zeit = 1e2;
+	double Zeit = 100.0;
 	double Breite = 1e-2;
-	double Messung = 0.0; 
-
-	Datei.open("Ergebnisse/Ergebnis_1.txt" , std::ios::trunc | std::ios::out);
-	Orte.open("Ergebnisse/Ergebnis_1_Orte.txt" , std::ios::trunc | std::ios::out);
-	OBS.open("Ergebnisse/Ergebnis_1_OBS.txt" , std::ios::trunc | std::ios::out);	
-	PAAR.open("Ergebnisse/Ergebnis_1_PAAR.txt" , std::ios::trunc | std::ios::out);
-	Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei , Orte , OBS , PAAR);
-	OBS.close();
-	Datei.close();
-	Orte.close();
-	PAAR.close();//*/
-
-
-	Datei.open("Ergebnisse/Ergebnis_2.txt" , std::ios::trunc | std::ios::out);
-	Orte.open("Ergebnisse/Ergebnis_2_Orte.txt" , std::ios::trunc | std::ios::out);
-	OBS.open("Ergebnisse/Ergebnis_2_OBS.txt" , std::ios::trunc | std::ios::out);
-	PAAR.open("Ergebnisse/Ergebnis_2_PAAR.txt" , std::ios::trunc | std::ios::out);
+	double Messung = 50.0; 
+	//Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei+"1" , false);
 
 	Tempe = 0.01;
 	Laenge = 8.0;
-	Zeit = 1e3;
-	Breite = 1e-3;
-	Messung = 0.0;
-
-	Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei , Orte , OBS , PAAR);
-	OBS.close();
-	Datei.close();
-	Orte.close();
-	PAAR.close();//*/
-
-
-	Datei.open("Ergebnisse/Ergebnis_3.txt" , std::ios::trunc | std::ios::out);
-	Orte.open("Ergebnisse/Ergebnis_3_Orte.txt" , std::ios::trunc | std::ios::out);
-	OBS.open("Ergebnisse/Ergebnis_3_OBS.txt" , std::ios::trunc | std::ios::out);
-	PAAR.open("Ergebnisse/Ergebnis_3_PAAR.txt" , std::ios::trunc | std::ios::out);
-
+	Zeit = 100.0;
+	Breite = 1e-2;
+	Messung = 60.0;
+	//Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei+"2" , false );
 
 	Tempe = 100.0;
 	Laenge = 8.0;
-	Zeit = 1e2;
+	Zeit = 2e2;
 	Breite = 1e-3;
-	Messung = 0.0;
-
-	Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei , Orte , OBS , PAAR);
-	OBS.close();
-	Datei.close();
-	Orte.close();
-	PAAR.close();//*/
-
-
-	Datei.open("Ergebnisse/Ergebnis_0.txt" , std::ios::trunc | std::ios::out);
-	Orte.open("Ergebnisse/Ergebnis_0_Orte.txt" , std::ios::trunc | std::ios::out);
-	OBS.open("Ergebnisse/Ergebnis_0_OBS.txt" , std::ios::trunc | std::ios::out);
-	PAAR.open("Ergebnisse/Ergebnis_0_PAAR.txt" , std::ios::trunc | std::ios::out);
+	Messung = 20.0;
+	//Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei+"3" , false );
 
 	Tempe = 1.0;
 	Laenge = 4.0;
-	Zeit = 1e1;
+	Zeit = 5.0;
+	Breite = 1e-3;
+	Messung = 2.0;
+	//Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei+"0" , false );
+
+	Tempe = 0.01;
+	Laenge = 8.0;
+	Zeit = 1.5e2;
 	Breite = 1e-2;
-	Messung = 0.0;
+	Messung = 50.0;
+	Verlet( Breite , Tempe , Laenge , Zeit , Bins , Messung , Datei+"D" , true );
+
 
 	return 0;
 }
