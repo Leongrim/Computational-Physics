20d600eb3e23fd7044f4a3082dce84c8422645ed
Leonard Wollenberg <Leonard.Wollenberg@tu-dortmund.de>
2016-06-10 12:02:54 +0200

    Plots Fertig

--- Blatt-07/plot.py
+++ Blatt-07/plot.py
@@ -6,11 +6,18 @@ def  GAUSS( x ):
 	return 1/np.sqrt(2*np.pi)*np.exp(-(x)**2/2)
 	pass
 
+xLabel = "Zufallszahl"
+yLabel = "Nomierter Bininhalt"
 
-Bin , Bincontent  = np.genfromtxt( 'Ergebnisse/Ergebnis_1_1_B.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( Bin , Bincontent , linestyle="steps")
+Xbin = np.arange(0 , 1.01 ,0.01)
+einzehntel = np.ones(101)
+Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_1_1_B.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Verteilung 1 gebinnte" )
+plt.plot(Xbin , einzehntel , "-r" , label="Theorie")
+#plt.ylim(0.09 , 0.11)
+plt.legend(loc='lower right')
 plt.savefig("Plots/Plot_1_1_B.pdf")
 plt.clf()
 
@@ -23,10 +30,12 @@ plt.clf()
 
 ######################################################################################################################
 
-Bin , Bincontent  = np.genfromtxt( 'Ergebnisse/Ergebnis_1_2_B.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( Bin , Bincontent , linestyle="steps")
+Bin , Bincontent , Nomiert  = np.genfromtxt( 'Ergebnisse/Ergebnis_1_2_B.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Verteilung 2 gebinnte" )
+plt.plot(Xbin , einzehntel , "-r" , label="Theorie")
+plt.legend(loc='lower right')
 plt.savefig("Plots/Plot_1_2_B.pdf")
 plt.clf()
 
@@ -39,10 +48,12 @@ plt.clf()
 
 ######################################################################################################################
 
-Bin , Bincontent  = np.genfromtxt( 'Ergebnisse/Ergebnis_1_3_B.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( Bin , Bincontent , linestyle="steps")
+Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_1_3_B.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Verteilung 3 gebinnte" )
+plt.plot(Xbin , einzehntel , "-r" , label="Theorie")
+plt.legend(loc='lower right')
 plt.savefig("Plots/Plot_1_3_B.pdf")
 plt.clf()
 
@@ -55,10 +66,12 @@ plt.clf()
 
 ######################################################################################################################
 
-Bin , Bincontent  = np.genfromtxt( 'Ergebnisse/Ergebnis_1_4_B.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( Bin , Bincontent , linestyle="steps")
+Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_1_4_B.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Verteilung 4 gebinnte" )
+plt.plot(Xbin , einzehntel , "-r" , label="Theorie")
+plt.legend(loc='lower right')
 plt.savefig("Plots/Plot_1_4_B.pdf")
 plt.clf()
 
@@ -72,35 +85,87 @@ plt.clf()
 
 x = np.arange(-6 , 6 , 0.01)
 
-Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_2_b.txt' , unpack=True)
+Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_2_a.txt' , unpack=True)
 plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( x , GAUSS(x))
-plt.plot( Bin , Nomiert , linestyle="steps")
+plt.ylabel("normierter Bininhalt")
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Gebinnte Zufallszahlen")
+plt.plot( x , GAUSS(x) , "-r" , label="Theorie")
 plt.grid()
+plt.legend(loc='best')
+plt.savefig("Plots/Plot_2_a.pdf")
+plt.clf()
+
+
+x = np.arange(-6 , 6 , 0.01)
+
+Bin_b , Bincontent_b , Nomiert_b = np.genfromtxt( 'Ergebnisse/Ergebnis_2_b.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+#plt.bar( Bin , Nomiert_b - GAUSS( Bin ) , width  = 0.1 )
+plt.bar( Bin , Nomiert - GAUSS( Bin ) , width  = 0.1 , label= "Numerik - Theorie" )
+plt.grid()
+plt.legend(loc='best')
+plt.savefig("Plots/Plot_2_a_diff.pdf")
+plt.clf()
+
+
+x = np.arange(-6 , 6 , 0.01)
+
+Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_2_b.txt' , unpack=True)
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.1 , label="Gebinnte Zufallszahlen")
+plt.plot( x , GAUSS(x) , "-r" , label="Theorie")
+plt.grid()
+plt.legend(loc='best')
 plt.savefig("Plots/Plot_2_b.pdf")
+plt.clf()
+
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert - GAUSS(Bin), width  = 0.1 , label= "Numerik - Theorie"  )
+plt.grid()
+plt.legend(loc='best')
+plt.savefig("Plots/Plot_2_b_diff.pdf")
 plt.clf()
 
 x = np.arange( 0 , np.pi , 0.01)
 
 Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_2_c.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( x , 0.5*np.sin(x))
-plt.plot( Bin , Nomiert , linestyle="steps")
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = np.pi/100 , label = "Gebinnte Zufallszahlen")
+plt.plot( x , 0.5*np.sin(x) , "-r" , label = "Theorie")
 plt.grid()
+plt.legend(loc='best')
 plt.savefig("Plots/Plot_2_c.pdf")
+plt.clf()
+
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert - 0.5*np.sin( Bin ), width  = np.pi/100 , label= "Numerik - Theorie"  )
+plt.grid()
+plt.legend(loc='best')
+plt.savefig("Plots/Plot_2_c_diff.pdf")
 plt.clf()
 
 
 x = np.arange( 0 , 1 , 0.01)
 
 Bin , Bincontent , Nomiert = np.genfromtxt( 'Ergebnisse/Ergebnis_2_d.txt' , unpack=True)
-plt.xlabel("Bin")
-plt.ylabel("Bininhalt")
-plt.plot( x , 3*x**2)
-plt.plot( x , Nomiert , linestyle="steps")
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert , width  = 0.01 , label = "Gebinnte Zufallszahlen")
+plt.plot( x , 3*x**2 , "-r" , label = "Theorie")
 plt.grid()
+plt.legend(loc='best')
 plt.savefig("Plots/Plot_2_d.pdf")
 plt.clf()
 
+plt.xlabel(xLabel)
+plt.ylabel(yLabel)
+plt.bar( Bin , Nomiert - 3*Bin**2, width  = 0.01 , label= "Numerik - Theorie"  )
+plt.grid()
+plt.legend(loc='best')
+plt.savefig("Plots/Plot_2_d_diff.pdf")
+plt.clf()
