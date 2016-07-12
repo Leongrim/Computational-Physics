fd3534eaead0db68e63e80675a1dc36aedfa73f9
Jan Doersch <jan.doersch@tu-dortmund.de>
2016-07-12 15:02:25 +0200

    Aufgabe 2 a fortgeschritten

--- Blatt-11/Aufgabe2.cpp
+++ Blatt-11/Aufgabe2.cpp
@@ -3,48 +3,141 @@
 #include <random>
 #include <fstream>
 
-//Monte-Carlo-Simulation eines einzelnen Spins
+//Monte-Carlo-Simulation des zweidimensionalen Ising-Modells
 
 std::mt19937 generator;
 
 double RANDOM(){//Zufallszahlengenerator für gleichverteilte Zufallszahlen zwischen 0 und 1
-	std::uniform_real_distribution<double> distribution(0.0 , 1.0);
+	std::uniform_real_distribution<double> distribution(0.0, 1.0);
 	return distribution(generator);
 }
 
+double RANDOM_int(int min, int max){//Zufallszahlengenerator für diskrete gleichverteilte Zufallszahlen von min bis max
+	std::uniform_int_distribution<int> distribution(min, max);
+	return distribution(generator);
+}
+
+double Hamilton(double spins[][100], int x_pos, int y_pos){//Berechnung der Energie
+	int x_plus = x_pos + 1;//rechter Nachbar
+	x_plus = x_plus - 100*floor(x_plus/100);//periodische Randbedingung
+	int x_minus = x_pos - 1;//linker Nachbar
+	x_minus = x_minus - 100*floor(x_minus/100);//periodische Randbedingung
+	int y_plus = y_pos + 1;//oberer Nachbar
+	y_plus = y_plus - 100*floor(y_plus/100);//periodische Randbedingung
+	int y_minus = y_pos - 1;//unterer Nachbar
+	y_minus = y_minus - 100*floor(y_minus/100);//periodische Randbedingung
+	double H = -spins[x_pos][y_pos] * (spins[x_minus][y_pos] + spins[x_plus][y_pos] + spins[x_pos][y_minus] + spins[x_pos][y_plus]);
+	//std::cout << H << "\n";
+	return H;
+}
+
 int main(){
-	//Initialisierung
-	double H = -5.0;//Beginne mit dem Magnetfeld H=-5
-	double m = 0.0;//Magnetisierung anfangs =0 für spätere Summation
-	double spin = 1.0;//Anfangszustand: Spin=+1
-	double deltaE;//Energiedifferenz
-	int H_Anzahl = 1e4;//Anzahl der zu durchlaufenden Magnetfeldwerte
-	int T = 1e5;//Anzahl der Zeitschritte
-	//Erzeuge Ausgabedatei zur Speicherung der Werte
 	std::fstream Datei;
-	Datei.open("Ergebnisse/Ergebnis_1_B.txt", std::ios::trunc | std::ios::out);
-	Datei << "#H\tm\n";
-	//Ziehe 10^4 Werte für H zwischen -5 und 5 mit je 10^5 Zeitschritten
-	for(int i = 0; i < H_Anzahl; ++i){//Schleife zum Durchlaufen der Magnetfeldwerte
-		for(int t = 0; t < T; ++t){//Schleife zum Durchlaufen der Zeitschritte
-			deltaE = 2.0*spin*H;//Berechne Energiedifferenz
-			if(deltaE < 0.0){//In diesem Fall immer akzeptieren
-				spin = -spin;//Zustand wird geändert
-			}
-			else if(deltaE > 0.0){//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
-				if(RANDOM() < exp(-deltaE)){//In diesem Fall wird akzeptiert
-					spin = -spin;//Zustand wird geändert
-				}
-				else;//sonst wird abgelehnt
-			}
-			m += spin;//Summation für Mittelung der Observablen, hier Magnetisierung
+	Datei.open("Ergebnisse/Ergebnis_2_A_geordnet_0.txt", std::ios::trunc | std::ios::out);
+	//Datei << "#Position_x\tPosition_y\tSpin\n";
+	//Initialisierung für geordneten Anfangszustand
+	double spins_geordnet[100][100];
+	for(int i = 0; i < 100; ++i){
+		for(int j = 0; j < 100; ++j){
+			spins_geordnet[i][j] = 1.0;
+			Datei << spins_geordnet[i][j] << "\t";
 		}
-		Datei << H << "\t" << m/(double)T << "\n";//Speichern in Datei
-		m = 0.0;//Zurücksetzen der Magnetisierung für erneute Messung (bei nächstem Magnetfeldwert)
-		H += 10.0/((double)H_Anzahl-1.0);//Schritt zu nächstem Magnetfeldwert
+		Datei << "\n";
+	}
+	Datei.close();
+
+	int t_MC = 10;//Anzahl der Zeitschritte
+	for(int t = 0; t < t_MC; ++t){//Schleife zum Durchlaufen der Zeitschritte
+		int x_pos = RANDOM_int(0, 99);
+		//std::cout << x_pos << "\n";
+		int y_pos = RANDOM_int(0, 99);
+		//std::cout << y_pos << "\n";
+		double deltaE = 0.0;
+		deltaE -= Hamilton(spins_geordnet, x_pos, y_pos);//Berechne Energiedifferenz
+		spins_geordnet[x_pos][y_pos] *= -1.0;
+		deltaE += Hamilton(spins_geordnet, x_pos, y_pos);
+		if(deltaE < 0.0){//In diesem Fall immer akzeptieren
+			//spins_geordnet[x_pos][y_pos] *= -1.0;
+			//spin = -spin;//Zustand wird geändert
+		}
+		else if(deltaE >= 0.0){//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
+			if(RANDOM() < exp(-deltaE)){//In diesem Fall wird akzeptiert
+				//spins_geordnet[x_pos][y_pos] *= -1.0;
+				//spin = -spin;//Zustand wird geändert
+			}
+			else{
+				spins_geordnet[x_pos][y_pos] *= -1.0;//sonst wird abgelehnt
+			}
+		}
 	}
 
+	Datei.open("Ergebnisse/Ergebnis_2_A_geordnet_10.txt", std::ios::trunc | std::ios::out);
+	for(int i = 0; i < 100; ++i){
+		for(int j = 0; j < 100; ++j){
+			Datei << spins_geordnet[i][j] << "\t";
+		}
+		Datei << "\n";
+	}
 	Datei.close();
+
+
+	Datei.open("Ergebnisse/Ergebnis_2_A_zufall_0.txt", std::ios::trunc | std::ios::out);
+	Datei << "#Position_x\tPosition_y\tSpin\n";
+	//Initialisierung für zufälligen Anfangszustand
+	double spins_zufall[100][100];
+	//int count_plus = 0;
+	//int count_minus = 0;
+	for(int i = 0; i < 100; ++i){
+		for(int j = 0; j < 100; ++j){
+			double rnd = RANDOM_int(0, 1);
+			if(rnd == 1){
+				spins_zufall[i][j] = 1.0;
+				//count_plus += 1;
+			}
+			else if(rnd == 0){
+				spins_zufall[i][j] = -1.0;
+				//count_minus += 1;
+			}
+			Datei << spins_zufall[i][j] << "\t";
+		}
+		Datei << "\n";
+	}
+	//std::cout << count_plus << "\t" << count_minus << std::endl;
+	Datei.close();
+
+	for(int t = 0; t < t_MC; ++t){//Schleife zum Durchlaufen der Zeitschritte
+		int x_pos = RANDOM_int(0, 99);
+		//std::cout << x_pos << "\n";
+		int y_pos = RANDOM_int(0, 99);
+		//std::cout << y_pos << "\n";
+		double deltaE = 0.0;
+		deltaE -= Hamilton(spins_zufall, x_pos, y_pos);//Berechne Energiedifferenz
+		spins_zufall[x_pos][y_pos] *= -1.0;
+		deltaE += Hamilton(spins_zufall, x_pos, y_pos);
+		if(deltaE < 0.0){//In diesem Fall immer akzeptieren
+			//spins_zufall[x_pos][y_pos] *= -1.0;
+			//spin = -spin;//Zustand wird geändert
+		}
+		else if(deltaE >= 0.0){//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
+			if(RANDOM() < exp(-deltaE)){//In diesem Fall wird akzeptiert
+				//spins_zufall[x_pos][y_pos] *= -1.0;
+				//spin = -spin;//Zustand wird geändert
+			}
+			else{
+				spins_zufall[x_pos][y_pos] *= -1.0;//sonst wird abgelehnt
+			}
+		}
+	}
+
+	Datei.open("Ergebnisse/Ergebnis_2_A_zufall_10.txt", std::ios::trunc | std::ios::out);
+	for(int i = 0; i < 100; ++i){
+		for(int j = 0; j < 100; ++j){
+			Datei << spins_zufall[i][j] << "\t";
+		}
+		Datei << "\n";
+	}
+	Datei.close();
+
 
 	return 0;
 }
--- Blatt-11/plot.py
+++ Blatt-11/plot.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
+from matplotlib import rc
+rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
+rc('text', usetex=True)
 
 x, y = np.genfromtxt("Ergebnisse/Ergebnis_1_B.txt", unpack=True)
 plt.plot(x, y, 'rx', label='$\mathrm{MC}$')
@@ -13,3 +16,20 @@ plt.xlabel(r'$H$')
 plt.ylabel(r'$m$')
 plt.legend(loc='best')
 plt.savefig("Magnetisierung.pdf")
+plt.close()
+
+for i in [0, 10, 100, 1000, 10000, 11000, 12000, 15000, 20000, 50000, 100000, 1000000, 10000000]:
+	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_geordnet_"+str(i)+".txt", unpack=True)
+	plt.imshow(Matrix, interpolation='nearest', vmin=-1, vmax=1)
+	plt.xlabel(r'$x$')
+	plt.ylabel(r'$y$')
+	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
+	plt.savefig("Plots/Momentaufnahme_geordnet_"+str(i)+".pdf")
+
+for i in [0, 10, 100, 1000, 10000, 11000, 12000, 15000, 20000, 50000, 100000, 1000000, 10000000]:
+	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_zufall_"+str(i)+".txt", unpack=True)
+	plt.imshow(Matrix, interpolation='nearest', vmin=-1, vmax=1)
+	plt.xlabel(r'$x$')
+	plt.ylabel(r'$y$')
+	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
+	plt.savefig("Plots/Momentaufnahme_zufall_"+str(i)+".pdf")
