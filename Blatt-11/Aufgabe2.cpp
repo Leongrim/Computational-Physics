c73f74b067a79bdcc5e0f55727f4fb7367c79e6a
Jan Doersch <jan.doersch@tu-dortmund.de>
2016-07-14 00:05:52 +0200

    Aufgabe 2 b) und c) fertig

--- Blatt-11/Aufgabe2.cpp
+++ Blatt-11/Aufgabe2.cpp
@@ -3,6 +3,7 @@
 #include <random>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 //Monte-Carlo-Simulation des zweidimensionalen Ising-Modells
 
@@ -28,15 +29,18 @@ double Hamilton(double spins[][100], int x_pos, int y_pos){//Berechnung der Ener
 	int y_minus = y_pos - 1;//unterer Nachbar
 	y_minus = y_minus - 100*floor(y_minus/100);//periodische Randbedingung
 	double H = -spins[x_pos][y_pos] * (spins[x_minus][y_pos] + spins[x_plus][y_pos] + spins[x_pos][y_minus] + spins[x_pos][y_plus]);
-	//std::cout << H << "\n";
 	return H;
 }
 
-void momentaufnahme(double spins[][100], const char* Dateiname, int Zeitschritte, double beta){
+void metropolis(double spins[][100], const char* Dateiname_Momentaufnahme, const char* Dateiname_Energie, std::fstream &stream, int t_MC, double kbt){
 	std::fstream Datei;
+	std::string KBoltzmannT = static_cast<std::ostringstream*>( &(std::ostringstream() << kbt) )->str();
 
-	double deltaE;
-	int t_MC = Zeitschritte;//Anzahl der Zeitschritte
+	double Energie = 0.0;
+	double deltaE = 0.0;
+	double m = 0.0;//Magnetisierung anfangs =0 für spätere Summation
+	//Datei.open(Dateiname_Energie + KBoltzmannT + ".txt", std::ios::trunc | std::ios::out);
+	//Datei << "#t/Sweeps\te(t)\tm\t|m|\n";
 	for(int t = 0; t < t_MC; ++t){//Schleife zum Durchlaufen der Zeitschritte
 		int x_pos = RANDOM_int(0, 99);
 		int y_pos = RANDOM_int(0, 99);
@@ -44,15 +48,28 @@ void momentaufnahme(double spins[][100], const char* Dateiname, int Zeitschritte
 		spins[x_pos][y_pos] *= -1.0;
 		deltaE += Hamilton(spins, x_pos, y_pos);
 		if(deltaE < 0.0){}//In diesem Fall immer akzeptieren
-		else if(deltaE >= 0.0){//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
-			if(RANDOM() < exp(-beta*deltaE)){}//In diesem Fall wird akzeptiert
+		else /*if(deltaE >= 0.0)*/{//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
+			if(RANDOM() < exp(-(1.0/kbt)*deltaE)){}//In diesem Fall wird akzeptiert
 			else{
 				spins[x_pos][y_pos] *= -1.0;//sonst wird abgelehnt
 			}
 		}
+		if(t % 10000 == 0 && t > 0){
+			for(int i = 0; i < 100; ++i){
+				for(int j = 0; j < 100; ++j){
+					Energie += Hamilton(spins, i, j);
+					m += spins[i][j];
+				}
+			}
+			//Datei << t/10000 << "\t" << Energie/(double)t << "\t" << m/(double)t << "\t" << std::abs(m/(double)t) << "\n";
+			std::cout << "In diesem Durchlauf sind " << 100*(double)t/(double)t_MC << " % geschafft.\r";
+		}
 	}
+	//Datei.close();
+	stream << kbt << "\t" << m/(double)t_MC << "\t" << std::abs(m/(double)t_MC) << "\n";
 
-	Datei.open(Dateiname, std::ios::trunc | std::ios::out);
+/*	std::string Zeitschritte = static_cast<std::ostringstream*>( &(std::ostringstream() << t_MC) )->str();
+	Datei.open(Dateiname_Momentaufnahme + Zeitschritte + "_kBT" + KBoltzmannT + ".txt", std::ios::trunc | std::ios::out);
 	for(int i = 0; i < 100; ++i){
 		for(int j = 0; j < 100; ++j){
 			Datei << spins[i][j] << "\t";
@@ -60,7 +77,40 @@ void momentaufnahme(double spins[][100], const char* Dateiname, int Zeitschritte
 		Datei << "\n";
 	}
 	Datei.close();
+*/}
 
+void metropolis_fluk(double spins[][100], std::fstream &stream, int t_MC, double kbt){
+	double Energie = 0.0;
+	double Energie2 = 0.0;
+	double deltaE = 0.0;
+	//Datei.open(Dateiname_Energie + KBoltzmannT + ".txt", std::ios::trunc | std::ios::out);
+	//Datei << "#t/Sweeps\te(t)\tm\t|m|\n";
+	for(int t = 0; t < t_MC; ++t){//Schleife zum Durchlaufen der Zeitschritte
+		int x_pos = RANDOM_int(0, 99);
+		int y_pos = RANDOM_int(0, 99);
+		deltaE = -Hamilton(spins, x_pos, y_pos);//Berechne Energiedifferenz
+		spins[x_pos][y_pos] *= -1.0;
+		deltaE += Hamilton(spins, x_pos, y_pos);
+		if(deltaE < 0.0){}//In diesem Fall immer akzeptieren
+		else /*if(deltaE >= 0.0)*/{//In diesem Fall Vergleich von e^(-beta deltaE) mit Zufallszahl RANDOM()
+			if(RANDOM() < exp(-(1.0/kbt)*deltaE)){}//In diesem Fall wird akzeptiert
+			else{
+				spins[x_pos][y_pos] *= -1.0;//sonst wird abgelehnt
+			}
+		}
+		if(t % 10000 == 0 && t > 0){
+			for(int i = 0; i < 100; ++i){
+				for(int j = 0; j < 100; ++j){
+					Energie += Hamilton(spins, i, j);
+					Energie2 += Hamilton(spins, i, j) * Hamilton(spins, i, j);
+				}
+			}
+			//std::cout << Energie/(double)t << "\n";
+			std::cout << "In diesem Durchlauf sind " << 100*(double)t/(double)t_MC << " % geschafft.\r";
+		}
+	}
+	std::cout << "\n" << kbt << "\t" << (Energie2/(double)t_MC - (Energie/(double)t_MC)*(Energie/(double)t_MC))/(kbt*kbt*10000.0) << "\n";
+	std::cout << "\n" << kbt << "\t" << Energie2 << "\t" << (double)t_MC << "\t" << Energie/(double)t_MC << "\n";
 }
 
 int main(){
@@ -76,25 +126,10 @@ int main(){
 		Datei << "\n";
 	}
 	Datei.close();
-
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10_kBT1.txt", 10, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t100_kBT1.txt", 100, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t1000_kBT1.txt", 1000, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10000_kBT1.txt", 10000, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t100000_kBT1.txt", 100000, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t1000000_kBT1.txt", 1000000, 1.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10000000_kBT1.txt", 10000000, 1.0);
-
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10_kBT3.txt", 10, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t100_kBT3.txt", 100, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t1000_kBT3.txt", 1000, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10000_kBT3.txt", 10000, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t100000_kBT3.txt", 100000, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t1000000_kBT3.txt", 1000000, 1.0/3.0);
-	momentaufnahme(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t10000000_kBT3.txt", 10000000, 1.0/3.0);
+	
+	int t_MC = 100000000;
 
 	Datei.open("Ergebnisse/Ergebnis_2_A_zufall_t0.txt", std::ios::trunc | std::ios::out);
-	Datei << "#Position_x\tPosition_y\tSpin\n";
 	//Initialisierung für zufälligen Anfangszustand
 	double spins_zufall[100][100];
 	for(int i = 0; i < 100; ++i){
@@ -112,22 +147,33 @@ int main(){
 	}
 	Datei.close();
 
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10_kBT1.txt", 10, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t100_kBT1.txt", 100, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t1000_kBT1.txt", 1000, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10000_kBT1.txt", 10000, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t100000_kBT1.txt", 100000, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t1000000_kBT1.txt", 1000000, 1.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10000000_kBT1.txt", 10000000, 1.0);
+/*	std::fstream Datei2;
+	Datei2.open("Ergebnisse/Ergebnis_2_C_Magnetisierung.txt", std::ios::trunc | std::ios::out);
+	Datei2 << "#kbt\tm\t|m|\n";
+	double kbt = 1.0;
+	for(int i = 0; i <= 200; ++i){
+		std::cout << "kBT = " << kbt << ".                                 \r\n";
+		metropolis(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t", "Ergebnisse/Ergebnis_2_B_Energie_geordnet_kBT", Datei2, t_MC, kbt);
+		kbt += 0.01;
+	}
+	Datei2.close();*/ 
+	//metropolis(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t", "Ergebnisse/Ergebnis_2_B_Energie_geordnet_kBT", t_MC, 2.27);
+	//metropolis(spins_geordnet, "Ergebnisse/Ergebnis_2_A_geordnet_t", "Ergebnisse/Ergebnis_2_B_Energie_geordnet_kBT", t_MC, 3.0);
 
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10_kBT3.txt", 10, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t100_kBT3.txt", 100, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t1000_kBT3.txt", 1000, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10000_kBT3.txt", 10000, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t100000_kBT3.txt", 100000, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t1000000_kBT3.txt", 1000000, 1.0/3.0);
-	momentaufnahme(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t10000000_kBT3.txt", 10000000, 1.0/3.0);
+	//metropolis(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t", "Ergebnisse/Ergebnis_2_B_Energie_zufall_kBT", t_MC, 1.0);
+	//metropolis(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t", "Ergebnisse/Ergebnis_2_B_Energie_zufall_kBT", t_MC, 2.27);
+	//metropolis(spins_zufall, "Ergebnisse/Ergebnis_2_A_zufall_t", "Ergebnisse/Ergebnis_2_B_Energie_zufall_kBT", t_MC, 3.0);
 
+	std::fstream Datei3;
+	Datei3.open("Ergebnisse/Ergebnis_2_D_Energiefluktuation.txt", std::ios::trunc | std::ios::out);
+	Datei3 << "#kBT\tc(T)\n";
+	double kbt = 1.0;
+	for(int i = 0; i <= 0; ++i){
+		std::cout << "kBT = " << kbt << ".                                   \r\n";
+		metropolis_fluk(spins_geordnet, Datei3, t_MC, kbt);
+		kbt += 0.01;
+	}
+	Datei3.close();
 
 	return 0;
 }
--- Blatt-11/plot.py
+++ Blatt-11/plot.py
@@ -24,6 +24,7 @@ plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
 plt.title(r'$t_\mathrm{MC}=0$')	
 plt.savefig("Plots/2_A_Momentaufnahme_geordnet_t0.pdf")
+plt.close()
 
 for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_geordnet_t"+str(i)+"_kBT1.txt", unpack=True)
@@ -32,6 +33,7 @@ for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	plt.ylabel(r'$y$')
 	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
 	plt.savefig("Plots/2_A_Momentaufnahme_geordnet_t"+str(i)+"_kBT1.pdf")
+	plt.close()
 
 for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_geordnet_t"+str(i)+"_kBT3.txt", unpack=True)
@@ -40,6 +42,7 @@ for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	plt.ylabel(r'$y$')
 	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
 	plt.savefig("Plots/2_A_Momentaufnahme_geordnet_t"+str(i)+"_kBT3.pdf")
+	plt.close()
 
 Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_zufall_t0.txt", unpack=True)
 plt.imshow(Matrix, interpolation='nearest', vmin=-1, vmax=1)
@@ -47,6 +50,7 @@ plt.xlabel(r'$x$')
 plt.ylabel(r'$y$')
 plt.title(r'$t_\mathrm{MC}=0$')	
 plt.savefig("Plots/2_A_Momentaufnahme_zufall_t0.pdf")
+plt.close()
 
 for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_zufall_t"+str(i)+"_kBT1.txt", unpack=True)
@@ -55,6 +59,7 @@ for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	plt.ylabel(r'$y$')
 	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
 	plt.savefig("Plots/2_A_Momentaufnahme_zufall_t"+str(i)+"_kBT1.pdf")
+	plt.close()
 
 for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	Matrix = np.genfromtxt("Ergebnisse/Ergebnis_2_A_zufall_t"+str(i)+"_kBT3.txt", unpack=True)
@@ -63,3 +68,67 @@ for i in [10, 100, 1000, 10000, 100000, 1000000, 10000000]:
 	plt.ylabel(r'$y$')
 	plt.title(r'$t_\mathrm{MC}=$'+str(i))	
 	plt.savefig("Plots/2_A_Momentaufnahme_zufall_t"+str(i)+"_kBT3.pdf")
+	plt.close()
+
+A = ["geordnet_kBT1", "geordnet_kBT2.27", "geordnet_kBT3", "zufall_kBT1", "zufall_kBT2.27", "zufall_kBT3"]
+'''for i in [0, 1, 2, 3, 4, 5]:
+	t, e, m, absm = np.genfromtxt("Ergebnisse/Ergebnis_2_B_Energie_"+A[i]+".txt", unpack=True)
+	plt.plot(t, e, 'r-')
+	plt.grid()
+	ax.get_xaxis().get_major_formatter().set_useOffset(False)
+	plt.xlabel(r'$t/\mathrm{Sweeps}$')
+	plt.ylabel(r'$e(t)$')
+	plt.savefig("Plots/2_B_Energie_"+A[i]+".pdf")
+	plt.close()
+
+	plt.plot(t, m, 'r-')
+	plt.grid()
+	plt.xlabel(r'$t/\mathrm{Sweeps}$')
+	plt.ylabel(r'$\langle m\rangle(t)$')
+	plt.savefig("Plots/2_B_Magnetisierung_"+A[i]+".pdf")
+	plt.close()
+
+	plt.plot(t, absm, 'r-')
+	plt.grid()
+	plt.xlabel(r'$t/\mathrm{Sweeps}$')
+	plt.ylabel(r'$\langle|m|\rangle(t)$')
+	plt.savefig("Plots/2_B_|Magnetisierung|_"+A[i]+".pdf")
+	plt.close()
+
+for i in [0, 1, 2]:
+	t1, e1, m1, absm1 = np.genfromtxt("Ergebnisse/Ergebnis_2_B_Energie_"+A[i]+".txt", unpack=True)
+	t2, e2, m2, absm2 = np.genfromtxt("Ergebnisse/Ergebnis_2_B_Energie_"+A[i+3]+".txt", unpack=True)
+	plt.plot(t1, e1, 'r-', label='$\mathrm{geordnet}$')
+	plt.plot(t2, e2, 'b-', label='$\mathrm{ungeordnet}$')
+	plt.grid()
+	plt.xlabel(r'$t/\mathrm{Sweeps}$')
+	plt.ylabel(r'$e(t)$')
+	plt.legend(loc='best')
+	plt.savefig("Plots/2_B_Energie_"+A[i]+A[i+3]+".pdf")
+	plt.close()
+'''
+kbt, m, absm = np.genfromtxt("Ergebnisse/Ergebnis_2_C_Magnetisierung.txt", unpack=True)
+plt.plot(kbt, m, 'rx', label='$\mathrm{geordnet}$')
+plt.grid()
+plt.xlabel(r'$k_\mathrm{B}T$')
+plt.ylabel(r'$\langle m\rangle(k_\mathrm{B}T)$')
+#plt.legend(loc='best')
+plt.savefig("Plots/2_C_Magnetisierung.pdf")
+plt.close()
+
+plt.plot(kbt, absm, 'rx', label='$\mathrm{geordnet}$')
+plt.grid()
+plt.xlabel(r'$k_\mathrm{B}T$')
+plt.ylabel(r'$\langle|m|\rangle(k_\mathrm{B}T)$')
+#plt.legend(loc='best')
+plt.savefig("Plots/2_C_|Magnetisierung|.pdf")
+plt.close()
+
+kbt, c = np.genfromtxt("Ergebnisse/Ergebnis_2_D_Energiefluktuation.txt", unpack=True)
+plt.plot(kbt, m, 'rx', label='$\mathrm{geordnet}$')
+plt.grid()
+plt.xlabel(r'$k_\mathrm{B}T$')
+plt.ylabel(r'$c(k_\mathrm{B}T)$')
+#plt.legend(loc='best')
+plt.savefig("Plots/2_D_Energiefluktuation.pdf")
+plt.close()
